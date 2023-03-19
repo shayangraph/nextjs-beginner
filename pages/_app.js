c10e43cd5193177819c18fadd27a89da5766bda2
shayan jafari <shayanja12345@gmail.com>
2023-03-19 17:57:08 +0330

    practice style component in nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "../style/global.css";
+import '../style/App.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/route/page.js
+++ pages/route/page.js
@@ -1,19 +1,21 @@
 import Link from "next/link";
 import Head from "next/head";
-
+import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Page</title>
-        <meta content="first content"/>
+        <meta content="first content" />
       </Head>
-      <div className="container">
-        <p>this is a test</p>
-        <br />
-        <Link href="/">this is a page js</Link>
-      </div>
+      <Layout>
+        <div className="container">
+          <p>this is a test</p>
+          <br />
+          <Link href="/">this is a page js</Link>
+        </div>
+      </Layout>
     </>
   );
 }
