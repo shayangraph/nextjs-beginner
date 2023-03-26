e9d8135c603edbd628ad6a018d545477314edbe5
shayan jafari <shayanja12345@gmail.com>
2023-03-26 19:09:37 +0430

    update dynamic routes & add link to index page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import { useRouter } from "next/router";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { Card, CardBody, CardHeader } from "reactstrap";
+import Date from "@/components/date";
+import Link from "next/link";
 
 export default function Home({ allPostsData }) {
   const router = useRouter();
@@ -18,17 +20,17 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
-
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <Card>
               <li className={utilStyles.listItem} key={id}>
                 <CardHeader> {title}</CardHeader>
-                <br />
                 <CardBody>
-                  {id}
+                  <Link href={`/posts/${id}`}>{title}</Link>
                   <br />
-                  {date}
+                  <small className={utilStyles.lightText}>
+                    <Date dateString={date} />
+                  </small>
                 </CardBody>
               </li>
             </Card>
